bb63313e927452c4b0ee96bca90a8d566de3145f
Noah A. John <noah.a.john@gmail.com>
2017-12-02 22:56:39 -0500

    Added rarity rolls, and /open route
    	* /open, rolls a pack of 3, with at least one rare.

--- server/modules/user/routes_v1.js
+++ server/modules/user/routes_v1.js
@@ -1,7 +1,33 @@
+const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
 const dataPath = path.join(__dirname, '..', '..', 'data', 'items.json');
 const data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+
+const items = {
+  common: data.filter((e) => e.rarity === 'common'),
+  rare: data.filter((e) => e.rarity === 'rare'),
+  epic: data.filter((e) => e.rarity === 'epic'),
+  legendary: data.filter((e) => e.rarity === 'legendary')
+};
+
+const ITEM_DROP_RATES = {
+  common: 72.00,
+  rare: 22.00,
+  epic: 5.00,
+  lengendary: 1.00
+};
+const sumValues = obj => Object.values(obj).reduce((a, b) => a + b);
+assert(sumValues(ITEM_DROP_RATES)-100 < Number.EPSILON);
+
+function rollForRarity(minTier) {
+  const roll = 100*Math.random();
+  console.log("rarity roll:"+ roll);
+  if (roll < ITEM_DROP_RATES.legendary || minTier === "legendary") return "legendary";
+  else if (roll < ITEM_DROP_RATES.epic || minTier === "epic") return "epic";
+  else if (roll < ITEM_DROP_RATES.rare || minTier === "rare") return "rare";
+  else return "common";
+}
 
 function randomInt (low, high) {
   return Math.floor(Math.random() * (high - low) + low);
@@ -10,16 +36,31 @@ function randomInt (low, high) {
 function home(req, res) {
   console.log("home");
   const resObj = {
-    msg: "hello world, try '/api/v1/roll' or /api/v1/roll?pretty"
+    msg: "hello world, try '/api/v1/roll' or /api/v1/open'. Add ?pretty for formatted JSON output."
   };
   res.json(resObj);
 }
 
-function roll(req, res, next) {
+function rollForItem(minTier) {
+  const rarity = rollForRarity(minTier);
+  const itemBucket = items[rarity];
+  assert(itemBucket && itemBucket.length > 0, "no "+rarity+" items found!");
   console.log("roll");
+  const indexRoll = randomInt(0, itemBucket.length);
+  return itemBucket[indexRoll];
+}
+
+function open(req, res, next) {
   try {
-    const indexRoll = randomInt(0, data.length);
-    res.json(data[indexRoll]);
+    res.json([rollForItem("rare"), rollForItem(), rollForItem()]);
+  } catch(e) {
+    next(e);
+  }
+}
+
+function roll(req, res, next) {
+  try {
+    res.json(rollForItem());
   } catch(e) {
     next(e); 
   }
@@ -27,6 +68,7 @@ function roll(req, res, next) {
 
 function setup(app) {
   app.get('/api/v1/roll', roll);
+  app.get('/api/v1/open', open);
   app.get('/', home);
 
 }
